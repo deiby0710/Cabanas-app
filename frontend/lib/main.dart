81a3809900ff1813f9d3bb3a1ecd801a27b9a59d
deiby0710 <deibyalejandro10@hotmail.com>
2025-10-17 09:06:24 -0500

    Connection with firebase

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/core/routes/app_router.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Inicializa Firebase con la configuración correcta
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
